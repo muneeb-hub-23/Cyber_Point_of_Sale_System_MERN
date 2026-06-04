ce9e70220a4c62cf150d0923b24b242564e1ca6a
Muneeb Baig <109512760+muneeb-hub-23@users.noreply.github.com>
2026-06-04 16:37:17 +0500

    Retrieved Latest Data and saved it

--- frontend_next_js/src/apirequests/apiaddress.js
+++ frontend_next_js/src/apirequests/apiaddress.js
@@ -1,3 +1,3 @@
-// const apiaddress = "http://172.16.100.1:4000"
-const apiaddress = "http://localhost:4005"
+const apiaddress = "http://172.16.100.1:4000"
+// const apiaddress = "http://localhost:4000"
 export default apiaddress
